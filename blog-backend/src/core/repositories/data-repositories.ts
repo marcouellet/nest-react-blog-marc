239c7e9333560d8bd1dcfc4ef13ee262c68374f5
marcouellet <marc.ouellet2@gmail.com>
2022-06-06 08:44:20 -0400

    remove import of Mongoose  from test suite

--- blog-backend/src/core/repositories/data-repositories.ts
+++ blog-backend/src/core/repositories/data-repositories.ts
@@ -1,10 +1,10 @@
 import { Injectable, Inject } from '@nestjs/common';
-import { IDataRepositories, IGenericDataRepository, GenericDataRepository } from '../../../../src/core/repositories';
-import { User } from '../../../../src/frameworks/data/mongo/model/user.model';
-import { Post } from '../../../../src/frameworks/data/mongo/model/post.model';
+import { IDataRepositories, IGenericDataRepository, GenericDataRepository } from './';
+import { User } from '../entities/user.entity';
+import { Post } from '../entities/post.entity';
 
 @Injectable()
-export class MongoDataRepositoriesMock extends IDataRepositories {
+export class DataRepositories extends IDataRepositories {
 
   override users: IGenericDataRepository<User>;
   override posts: IGenericDataRepository<Post>;
--- blog-backend/test/mock/data/mongo/mongo-data.module.mock.ts
+++ blog-backend/test/mock/data/mongo/mongo-data.module.mock.ts
@@ -1,36 +1,15 @@
 import { Module, Global } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { IDataRepositories } from '../../../../src/core/repositories';
-import { User, UserSchema } from '../../../../src/frameworks/data/mongo/model/user.model';
-import { Post, PostSchema } from '../../../../src/frameworks/data/mongo/model/post.model';
-import { IConfigService } from '../../../../src/config/interfaces/config.interface';
-import { ConfigModule } from '../../../../src/modules/config.module';
-import { MongoDataRepositoriesMock } from './mongo-data-repositories.mock';
+import { IDataRepositories, DataRepositories } from '../../../../src/core/repositories';
 import UserRepositoryProvider from '../../../providers/user.repository.provider';
 import PostRepositoryProvider from '../../../providers/post.repository.provider';
-import { GLOBAL_TEST_CONFIG_SERVICE } from '../../../config/config.global';
-
 @Global()
 @Module({
-  imports: [
-    ConfigModule.register(GLOBAL_TEST_CONFIG_SERVICE),
-    MongooseModule.forRootAsync({
-      useFactory: async (configService: IConfigService) => ({
-        uri: configService.getConfig().connectionString,
-      }),
-      inject: [IConfigService],
-    }),
-    MongooseModule.forFeature([
-      { name: User.name, schema: UserSchema },
-      { name: Post.name, schema: PostSchema },
-    ]),
-  ],
   providers: [
     UserRepositoryProvider,
     PostRepositoryProvider,
     {
       provide: IDataRepositories,
-      useClass: MongoDataRepositoriesMock,
+      useClass: DataRepositories,
     },
 ],
   exports: [IDataRepositories],
