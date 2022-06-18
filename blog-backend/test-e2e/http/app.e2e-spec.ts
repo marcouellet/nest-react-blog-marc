0366261a5d6676777e9eb82d3647fdc00563ad24
marcouellet <marc.ouellet2@gmail.com>
2022-06-18 17:44:40 -0400

    AppController E2E setup

--- blog-backend/test-e2e/http/app.e2e-spec.ts
+++ blog-backend/test-e2e/http/app.e2e-spec.ts
@@ -2,17 +2,24 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../../src/modules/app.module';
+import { CustomLogger } from '../../src/common/custom.logger';
 import { GLOBAL_TEST_E2E_CONFIG_SERVICE } from '../config/config.global';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule.register(GLOBAL_TEST_E2E_CONFIG_SERVICE)],
-    }).compile();
+  CustomLogger.setGlobalPrefix('AppController E2E Tests');
 
-    app = moduleFixture.createNestApplication();
+  beforeEach(async () => {
+
+    const moduleBuilder = await Test.createTestingModule({
+      imports: [AppModule.register(GLOBAL_TEST_E2E_CONFIG_SERVICE)],
+    });
+
+    moduleBuilder.setLogger(new CustomLogger());
+    const appModule: TestingModule = await moduleBuilder.compile();
+
+    app = appModule.createNestApplication();
     await app.init();
   });
 
