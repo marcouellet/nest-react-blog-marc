7914d89198b1f52a69701da2f017a168d2b2572a
marcouellet <marc.ouellet2@gmail.com>
2022-07-04 07:24:50 -0400

    fix create post exception

--- blog-frontend-axios/src/routes/AppRoutes.tsx
+++ blog-frontend-axios/src/routes/AppRoutes.tsx
@@ -1,29 +1,38 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "../components/Home";
 import ListCategories from "../components/category/ListCategories";
 import ViewCategory from "../components/category/ViewCategory";
 import ViewPost from "../components/post/ViewPost";
+import CreatePost from "../components/post/CreatePost";
 import useAuth from '../contexts/auth';
 import PrivateRoutes from "./PrivateRoutes";
 import RestrictedRoutes from "./RestrictedRoutes";
 // import { UserRole } from '../types';
 import AdminRoutes from "./AdminRoutes";
+import { JsxAttributeLike } from "typescript";
 
 export const AppRoutes = () => {
 
   const { state : { isAuthenticated } } = useAuth();
+  const [controlledRoutes, setControlledRoutes] = useState<JSX.Element>();
+
   // const isAdministrator = () => isAuthenticated && user?.role === UserRole.ADMIN;
+
+  useEffect(() => {
+    setControlledRoutes(isAuthenticated ?  PrivateRoutes() : RestrictedRoutes())
+  // eslint-disable-next-line
+  }, [isAuthenticated]);
 
   return (
     <Routes>
+      {controlledRoutes}
+      {AdminRoutes()}
       <Route path={"/"} element={<Home/>} />
+      <Route path={"/post/create"} element={<CreatePost/>} /> 
       <Route path={"/post/:postId"} element={<ViewPost/>}/>
       <Route path={"/category"} element={<ListCategories/>}/>
       <Route path={"/category/:categoryId"} element={<ViewCategory/>}/>
-      {isAuthenticated ?  PrivateRoutes() : RestrictedRoutes()}
-      {AdminRoutes()}
-      {/* {isAdministrator() ? AdminRoutes() : <></>} */}
       <Route
           path="*"
           element={<Navigate to="/" replace />}
--- blog-frontend-axios/src/routes/PrivateRoutes.tsx
+++ blog-frontend-axios/src/routes/PrivateRoutes.tsx
@@ -1,18 +1,10 @@
 import * as React from "react";
-import useAuth from '../contexts/auth';
 import { Route } from "react-router-dom";
 import UserProfile from "../components/user/UserProfile";
 import CreatePost from "../components/post/CreatePost";
 import EditPost from "../components/post/EditPost";
 
 const PrivateRoutes = () => {
-
-  const { state : { isAuthenticated } } = useAuth();
-
-  if (!isAuthenticated) {
-    return <></>
-  }
-
   return (
     <Route>
       <Route path={"/user/profile"} element={<UserProfile/>} />
