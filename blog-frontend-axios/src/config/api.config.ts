5f0c8ab64f69ff7c979d4b546edfd6c99fd3c128
marcouellet <marc.ouellet2@gmail.com>
2022-07-24 10:30:09 -0400

    fix docker

--- blog-frontend-axios/src/config/api.config.ts
+++ blog-frontend-axios/src/config/api.config.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = `${process.env.REACT_APP_API_URI}`;
-export const API_REQUEST_TIMEOUT = 1000000; // 10 secs
-export const HTTP_RESPONSE_HEADER_TIMESTAMP = 'http_response_header_timestamp'; 
+export const REACT_APP_API_BASE_URL = `${process.env.REACT_APP_API_URI}`;
+export const REACT_APP_API_REQUEST_TIMEOUT = 1000000; // 10 secs
+export const REACT_APP_HTTP_RESPONSE_HEADER_TIMESTAMP = 'http_response_header_timestamp'; 
