5fc59feb2612f64ef671df46a02a8a07c1dbed57
marcouellet <marc.ouellet2@gmail.com>
2022-08-07 09:43:48 -0400

    added trace for calls of useEffect - No duplicate call

--- blog-frontend-axios/src/components/blog/ListBlogs.tsx
+++ blog-frontend-axios/src/components/blog/ListBlogs.tsx
@@ -32,32 +32,33 @@ const ListBlogs = () => {
 
   useEffect(() => {
     (async () => {
+      // alert('listBlogs useEffet called');
       dispatchUI(createActionLoading(true));
       const fetchCategories = async (): Promise<void> => {
         await getDefaultPostImage()
-        .then(imageData => { setpostDefaultImage(imageData);})
-        .catch(error => {
-          throw new Error(error);
+          .then(imageData => { setpostDefaultImage(imageData);})
+          .catch(error => {
+            throw new Error(error);
         });   
         await getDefaultUserImage()
-        .then(imageData => { setuserDefaultImage(imageData);})
-        .catch(error => {
-          throw new Error(error);
+          .then(imageData => { setuserDefaultImage(imageData);})
+          .catch(error => {
+            throw new Error(error);
         });   
         await CategoryApiService.getAllCategories()
-        .then(categories => {
-          const all: ICategory = {id:'all', title: 'All', description: ''};
-          const noCategory: ICategory = {id:'no_category', title: 'No category', description: ''};
-          const allCategories = [all, noCategory].concat(categories);
-          setCategories(allCategories);
-          if (categoryFilter) {
-            selectCategory(allCategories, categoryFilter.id!, false);
-          } else {
-            selectCategory(allCategories, 'all', false);
-          }            
-        })
-        .catch((apiErrors: IErrors) => handleApiErrors(apiErrors,'Categories reading'));
-      }
+          .then(categories => {
+            const all: ICategory = {id:'all', title: 'All', description: ''};
+            const noCategory: ICategory = {id:'no_category', title: 'No category', description: ''};
+            const allCategories = [all, noCategory].concat(categories);
+            setCategories(allCategories);
+            if (categoryFilter) {
+              selectCategory(allCategories, categoryFilter.id!, false);
+            } else {
+              selectCategory(allCategories, 'all', false);
+            }            
+          })
+          .catch((apiErrors: IErrors) => handleApiErrors(apiErrors,'Categories reading'));
+        }
       await fetchCategories();  
     })().finally(() => dispatchUI(createActionLoading(false)));
  // eslint-disable-next-line
