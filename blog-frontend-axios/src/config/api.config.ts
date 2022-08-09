0f0b55420052390577e1b29276cd08c775ff38b4
marcouellet <marc.ouellet2@gmail.com>
2022-08-09 11:18:41 -0400

    rename env var

--- blog-frontend-axios/src/config/api.config.ts
+++ blog-frontend-axios/src/config/api.config.ts
@@ -1,3 +1,3 @@
-export const REACT_APP_API_BASE_URL = `${process.env.REACT_APP_API_URI}`;
+export const REACT_APP_API_BASE_URL = `${process.env.REACT_APP_BLOG_MARC_API_URI}`;
 export const REACT_APP_API_REQUEST_TIMEOUT = 1000000; // 10 secs
 export const REACT_APP_HTTP_RESPONSE_HEADER_TIMESTAMP = 'http_response_header_timestamp'; 
